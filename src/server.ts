1badb761640ebd6ec081ed37ad38d936e0526992
Shuaib Shaban <shabanshuaib454@gmail.com>
2024-07-11 10:47:22 +0300

    project setup:prisma orm

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import express = require('express');
+import router from './router'
+import morgan from 'morgan'
+
+const app = express();
+
+app.use(morgan('dev'))
+app.use(express.json)
+app.use(express.urlencoded({extended: true}))
+
+app.get('/', (req, res) => {
+    console.log('hello from express')
+    res.status(200)
+    res.json({message: 'hello'})
+})
+
+app.use('/api', router)
+ 
+export default app;
